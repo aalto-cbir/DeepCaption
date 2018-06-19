70c1e5542b3121db96776cc05a1226c2a6f45d91
Mats Sjöberg <mats.sjoberg@helsinki.fi>
2018-06-19 17:28:23 +0300

    Submission script for csc

---
+++ submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:p100:1
+#SBATCH --mem=8GB
+#SBATCH --time=0-1
+
+# Set billing project
+newgrp mvsjober
+
+# Load modules
+# module purge
+# module load python-env/3.5.3-ml
+# module list
+
+# research-support@csc.fi suggested for pytorch 0.4.0
+module purge
+module load python-env/intelpython3.6-2018.3
+module load gcc/5.4.0 cuda/9.0 cudnn/7.1-cuda9
+
+# Launch python script
+srun python3.6 $*
