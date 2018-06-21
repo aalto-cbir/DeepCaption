c2f11e10661f14015ea2c444cbd6ae56cb0574d6
Arturs Polis <arturs.polis@helsinki.fi>
2018-06-21 17:25:59 +0300

    added zip file handling

--- train.py
+++ train.py
@@ -7,8 +7,9 @@ import numpy as np
 import os
 import pickle
 import sys
+import zipfile
 
-from data_loader import get_loader 
+from data_loader import get_loader
 from build_vocab import Vocabulary
 from datetime import datetime
 from model import ModelParams, EncoderCNN, DecoderRNN
@@ -18,12 +19,13 @@ from torchvision import transforms
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
 def save_models(args, params, encoder, decoder, optimizer, epoch):
     bn = args.model_basename
-    file_name = '{}-ep{}.ckpt'.format(bn, epoch+1)
+    file_name = '{}-ep{}.ckpt'.format(bn, epoch + 1)
 
     state = {
-        'epoch': epoch+1,
+        'epoch': epoch + 1,
         'encoder': encoder.state_dict(),
         'decoder': decoder.state_dict(),
         'optimizer': optimizer.state_dict(),
@@ -35,29 +37,39 @@ def save_models(args, params, encoder, decoder, optimizer, epoch):
 
     torch.save(state, os.path.join(args.model_path, file_name))
 
-    
+
 def main(args):
     # Create model directory
     if not os.path.exists(args.model_path):
         os.makedirs(args.model_path)
-    
+
+    # Unzip training images to /tmp/data if image_dir argument points to ZIP file:
+    if zipfile.is_zipfile(args.image_dir):
+        data_prefix = '/tmp/data'
+        if not os.path.exists(data_prefix):
+            os.makedirs(data_prefix)
+        with zipfile.ZipFile(args.image_dir, 'r') as zipped_images:
+            zipped_images.extractall(data_prefix)
+            unzipped_dir = os.path.basename(args.image_dir).split('.')[0]
+            args.image_dir = data_prefix + unzipped_dir
+
     # Image preprocessing, normalization for the pretrained resnet
     transform = transforms.Compose([
         # transforms.Resize((256, 256)),
         transforms.RandomCrop(args.crop_size),
-        transforms.RandomHorizontalFlip(), 
-        transforms.ToTensor(), 
-        transforms.Normalize((0.485, 0.456, 0.406), 
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406),
                              (0.229, 0.224, 0.225))])
-    
+
     # Load vocabulary wrapper
     with open(args.vocab_path, 'rb') as f:
         vocab = pickle.load(f)
-    
+
     # Build data loader
-    data_loader = get_loader(args.image_dir, args.caption_path, vocab, 
+    data_loader = get_loader(args.image_dir, args.caption_path, vocab,
                              transform, args.batch_size,
-                             shuffle=True, num_workers=args.num_workers) 
+                             shuffle=True, num_workers=args.num_workers)
 
     state = None
     params = ModelParams.fromargs(args)
@@ -67,11 +79,11 @@ def main(args):
         state = torch.load(args.load_model)
         params = ModelParams(state)
         start_epoch = state['epoch']
-        print('Loading model {} at epoch {}.'.format(args.load_model, 
+        print('Loading model {} at epoch {}.'.format(args.load_model,
                                                      start_epoch))
     if args.force_epoch:
-        start_epoch = args.force_epoch-1
-    
+        start_epoch = args.force_epoch - 1
+
     # Build the models
     encoder = EncoderCNN(params).to(device)
     decoder = DecoderRNN(params, len(vocab)).to(device)
@@ -81,8 +93,8 @@ def main(args):
 
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss()
-    opt_params = (list(decoder.parameters()) + 
-                  list(encoder.linear.parameters()) + 
+    opt_params = (list(decoder.parameters()) +
+                  list(encoder.linear.parameters()) +
                   list(encoder.bn.parameters()))
     optimizer = torch.optim.Adam(opt_params, lr=args.learning_rate)
     if state:
@@ -96,7 +108,7 @@ def main(args):
             images = images.to(device)
             captions = captions.to(device)
             targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
-            
+
             # Forward, backward and optimize
             features = encoder(images)
             outputs = decoder(features, captions, lengths)
@@ -107,13 +119,14 @@ def main(args):
             optimizer.step()
 
             # Print log info
-            if (i+1) % args.log_step == 0:
+            if (i + 1) % args.log_step == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
-                      .format(epoch+1, args.num_epochs, i+1, total_step, 
-                              loss.item(), np.exp(loss.item()))) 
+                      .format(epoch + 1, args.num_epochs, i + 1, total_step,
+                              loss.item(), np.exp(loss.item())))
                 sys.stdout.flush()
-                
+
         save_models(args, params, encoder, decoder, optimizer, epoch)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -121,26 +134,28 @@ if __name__ == '__main__':
                         help='existing model, for continuing training')
     parser.add_argument('--model_basename', type=str, default='model',
                         help='base name for model snapshot filenames')
-    parser.add_argument('--model_path', type=str, default='models/' , 
+    parser.add_argument('--model_path', type=str, default='models/',
                         help='path for saving trained models')
-    parser.add_argument('--crop_size', type=int, default=224, 
+    parser.add_argument('--crop_size', type=int, default=224,
                         help='size for randomly cropping images')
     parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl',
                         help='path for vocabulary wrapper')
-    parser.add_argument('--image_dir', type=str, default='data/resized2014', 
-                        help='directory for resized images')
+    parser.add_argument('--image_dir', type=str, default='data/resized2014',
+                        help='directory for resized images'
+                        'if "image_dir" ends with ".zip" extract zip file '
+                        'to /tmp/ , use the extracted images to train')
     parser.add_argument('--caption_path', type=str,
-                        default='data/annotations/captions_train2014.json', 
+                        default='data/annotations/captions_train2014.json',
                         help='path for train annotation json file')
-    parser.add_argument('--log_step', type=int, default=10, 
+    parser.add_argument('--log_step', type=int, default=10,
                         help='step size for prining log info')
-    
+
     # Model parameters
-    parser.add_argument('--embed_size', type=int , default=256, 
+    parser.add_argument('--embed_size', type=int, default=256,
                         help='dimension of word embedding vectors')
-    parser.add_argument('--hidden_size', type=int , default=512, 
+    parser.add_argument('--hidden_size', type=int, default=512,
                         help='dimension of lstm hidden states')
-    parser.add_argument('--num_layers', type=int, default=1, 
+    parser.add_argument('--num_layers', type=int, default=1,
                         help='number of layers in lstm')
     parser.add_argument('--dropout', type=float, default=0,
                         help='dropout for the LSTM')
@@ -157,9 +172,10 @@ if __name__ == '__main__':
 
     begin = datetime.now()
     print('Started training at {}, with parameters:'.format(begin))
-    for k, v in vars(args).items(): print('[args] {}={}'.format(k, v))
+    for k, v in vars(args).items():
+        print('[args] {}={}'.format(k, v))
 
     main(args)
 
     end = datetime.now()
-    print('Training ended at {}. Total training time: {}.'.format(end, end-begin))
+    print('Training ended at {}. Total training time: {}.'.format(end, end - begin))
