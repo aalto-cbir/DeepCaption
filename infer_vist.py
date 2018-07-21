2a2634b17402db7770052ed9c1311059d9e61994
Aditya Kaushik <aditya.surikuchi@aalto.fi>
2018-07-21 17:43:55 +0300

    initial inference complete

--- infer_vist.py
+++ infer_vist.py
@@ -66,6 +66,7 @@ def main(args):
 
     # image preprocessing
     transform = transforms.Compose([
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406),
                              (0.229, 0.224, 0.225))])
