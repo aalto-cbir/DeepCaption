22b0e89079bf053652f645f411af2f92fc3d0b87
Sjöberg Mats <mats.sjoberg@aalto.fi>
2018-08-07 17:16:02 +0300

    plot_stats.py to work with several stats files

--- plot_stats.py
+++ plot_stats.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python3
 
 import json
+import os
+
 import matplotlib.pyplot as plt
 
 
-def plot_stats(stats):
+def plot_stats(label, stats, color):
     epochs = sorted([int(x) for x in stats.keys()])
 
     measures = list(stats[str(epochs[0])].keys())
 
     for measure in measures:
         vals = [stats[str(e)][measure] for e in epochs]
-        plt.plot(epochs, vals, label=measure)
-
-    plt.legend()
-    plt.show()
+        if measure == 'validation_loss':
+            linestyle = '--'
+        else:
+            linestyle = '-'
+        plt.plot(epochs, vals, label='{}: {}'.format(label, measure), color=color,
+                 linestyle=linestyle, linewidth=2)
 
 
 def main(args):
-    with open(args.stats_file, 'r') as fp:
-        plot_stats(json.load(fp))
+    colors = ['blue', 'red', 'cyan']
+    labels = args.labels.split(',') if args.labels else []
+
+    for i, filename in enumerate(args.files):
+        with open(filename, 'r') as fp:
+            if i < len(labels):
+                label = labels[i]
+            else:
+                label = str(i+1)
+            plot_stats(label, json.load(fp), colors[i % len(colors)])
+
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('stats_file', type=str, help='JSON file with training stats')
+    parser.add_argument('files', type=str, nargs='+',
+                        help='JSON file(s) with training stats')
+    parser.add_argument('--labels', type=str, help='Labels for plot')
     args = parser.parse_args()
 
     main(args)
