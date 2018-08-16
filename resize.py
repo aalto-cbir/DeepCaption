25db692421a63e54c4ef4882982d7638684a4352
Sjöberg Mats <mats.sjoberg@aalto.fi>
2018-08-16 10:43:41 +0300

    Cleaned up resizing code, progress display fixed

--- resize.py
+++ resize.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import os
 import cv2
@@ -5,7 +7,6 @@ import shutil
 import pymp
 import multiprocessing
 import glob
-from tqdm import tqdm
 
 
 def resize_image(image, size):
@@ -22,60 +23,46 @@ def resize_images(image_dir, output_dir, create_zip, size):
     'create_zip' tells whether we need to create a ZIP archive"""
     os.makedirs(output_dir, exist_ok=True)
 
-    images = os.listdir(image_dir)
+    if args.subset:
+        print('Resizing image subset defined in {} ...'.format(args.subset))
+        subset_ids = [line.rstrip() for line in open(args.subset, 'r')]
+        num_images = len(subset_ids)
+        images = []
+        for img_id in subset_ids:
+            # Attempt to auto-detect file extension:
+            img_ext = glob.glob(os.path.join(image_dir, img_id) + '.*')[0].split('.')[-1]
+            images.append("{}.{}".format(img_id, img_ext))
+    else:
+        print('Resizing all images...')
+        images = os.listdir(image_dir)
     num_images = len(images)
 
     # Run the job on several cores to speed it up:
     num_cores = min(4, multiprocessing.cpu_count())
     print('Using {} CPU cores'.format(num_cores))
 
-    # If no subset is specified:
-    if not args.subset:
-        print('Resizing all images...')
-        with pymp.Parallel(num_cores) as p:
-            for i in p.range(0, num_images):
-                image = images[i]
-                output_path = os.path.join(output_dir, image)
-                if os.path.isfile(output_path) and os.path.getsize(output_path) > 0:
-                    print("{} exists, skipping...".format(output_path))
-                    continue
-
-                img = cv2.imread(os.path.join(image_dir, image))
-                if img is not None:
-                    img = resize_image(img, size)
-                    cv2.imwrite(output_path, img)
-
-                if (i + 1) % 100 == 0:
-                    print("[{}/{}] Resized the images and saved into '{}'."
-                          .format(i + 1, num_images, output_dir))
-    # Load images defined as new-line separated subset of image-ids:
-    else:
-        print('Resizing image subset defined in {} ...'.format(args.subset))
-        subset_ids = [line.rstrip() for line in open(args.subset)]
-        num_images = len(subset_ids)
-        pbar = tqdm(total=num_images)
-        with pymp.Parallel(num_cores) as p:
-            for i in p.range(0, num_images):
-                img_id = subset_ids[i]
-                # Attempt to auto-detect file extension:
-                img_ext = glob.glob(os.path.join(image_dir, img_id) + '.*')[0].split('.')[-1]
-                image = "{}.{}".format(img_id, img_ext)
-                output_path = os.path.join(output_dir, image)
-                if os.path.isfile(output_path) and os.path.getsize(output_path) > 0:
-                    print("{} exists, skipping...".format(output_path))
-                    continue
-
+    counter = pymp.shared.array((1,), dtype='uint32')
+    with pymp.Parallel(num_cores) as p:
+        for i in p.range(num_images):
+            image = images[i]
+            output_path = os.path.join(output_dir, image)
+            if os.path.isfile(output_path) and os.path.getsize(output_path) > 0:
+                print("{} exists, skipping...".format(output_path))
+            else:
                 img = cv2.imread(os.path.join(image_dir, image))
                 if img is not None:
                     img = resize_image(img, size)
                     cv2.imwrite(output_path, img)
                 else:
-                    print('image {} not found or corrupted'.format(os.path.join(image_dir, image)))
+                    print('image {} not found or corrupted'.format(os.path.join(image_dir,
+                                                                                image)))
 
-                # if (i + 1) % 100 == 0:
-                #    print("[{}/{}] Resized the images and saved into '{}'."
-                #          .format(i + 1, num_images, output_dir))
-                pbar.update(1)
+            with p.lock:
+                counter[0] += 1
+                i = int(counter[0])
+                if (i + 1) % 100 == 0:
+                    print("[{}/{}] Resized the images and saved into '{}'."
+                          .format(i + 1, num_images, output_dir))
 
     if create_zip:
         print("Creating a zip file: {}".format(output_dir + '.zip'))
@@ -94,14 +81,12 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_dir', type=str,
-                        default='datasets/data/COCO/images/train2014',
+    parser.add_argument('--image_dir', type=str, required=True,
                         help='directory for train images')
     parser.add_argument('--subset', type=str, default=None,
                         help='path to (optional) new-line separated file '
                              'listing ids of images to include')
-    parser.add_argument('--output_dir', type=str,
-                        default='datasets/processed/COCO/train2014_resized',
+    parser.add_argument('--output_dir', type=str, required=True,
                         help='directory for saving resized images')
     parser.add_argument('--create_zip', action="store_true",
                         help='save ZIP file as "\{output_dir\}.zip"')
