52001a73146ab0cc3de5a6b477ee0a579bef1a67
Arturs Polis <arturs.polis@helsinki.fi>
2018-08-24 17:45:52 +0300

    fixed image size paramater bug

--- extract_dataset_features.py
+++ extract_dataset_features.py
@@ -23,7 +23,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 def main(args):
     # Image preprocessing
     transform = transforms.Compose([
-        transforms.Resize((args.crop_size, args.crop_size)),
+        transforms.Resize((args.image_size, args.image_size)),
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406),
                              (0.229, 0.224, 0.225))])
