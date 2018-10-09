c2f2857e979d7210884c1d2edf9845c64635651e
Jorma Laaksonen <jorma.laaksonen@aalto.fi>
2018-10-09 15:29:13 +0300

    Added add_special_tokens(), save() and get_list() methods.
    Added get_vocab_from_txt() and get_vocab_from_list().

--- vocabulary.py
+++ vocabulary.py
@@ -23,7 +23,20 @@ class Vocabulary(object):
     def __len__(self):
         return len(self.word2idx)
 
+    def add_special_tokens(self):
+        self.add_word('<pad>')
+        self.add_word('<start>')
+        self.add_word('<end>')
+        self.add_word('<unk>')
 
+    def save(self, vocab_path):
+        with open(vocab_path, 'wb') as f:
+            pickle.dump(self, f)
+
+    def get_list(self):
+        return [ self.idx2word[i] for i in range(self.__len__()) ]
+    
+            
 def get_vocab(vocab_path):
     # Load vocabulary wrapper
     with open(vocab_path, 'rb') as f:
@@ -31,3 +44,24 @@ def get_vocab(vocab_path):
         vocab = pickle.load(f)
 
     return vocab
+
+
+def get_vocab_from_txt(vocab_path):
+    l = []
+    with open(vocab_path) as f:
+        print("Extracting vocabulary from {} text file".format(vocab_path))
+        for a in f:
+            b = a.split()
+            l.extend(b)
+    return get_vocab_from_list(l, True)
+
+
+def get_vocab_from_list(l, add_specials):
+    vocab = Vocabulary()
+    if add_specials:
+        vocab.add_special_tokens()
+    for i in l:
+        vocab.add_word(i)
+
+    return vocab
+
