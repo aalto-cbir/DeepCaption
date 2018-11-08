2e26f3fbd001d7aaad58e38fe217c3ade3a04e85
Arturs Polis <arturs.polis@helsinki.fi>
2018-11-08 13:41:37 +0200

    rnn initialization test updated

--- tests/rnn_init_tests.sh
+++ tests/rnn_init_tests.sh
@@ -5,17 +5,9 @@ INFER_PY=$2
 
 # Train baseline model with rnn initialized from image features
 
-MODEL="coco-max-default-nl2-skip_start_token"
+MODEL="coco-max-default-nl2-rnn_init_tests"
 
-# Command where we do not disable <start> token generation should fail:
-python3 $TRAIN_PY --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 15  \
-                 --num_layers 2 --dropout 0.2 --vocab AUTO \
-                 --features coco\:train2014+coco\:val2014-resnet152-max-normalize-default.lmdb \
-                 --validate coco:val2014 \
-                 --model_name EncoderDecoder/test/${MODEL} \
-                 --rnn_hidden_init from_features 
-
-# The following should work:
+# Command where we disable <start> token generation should fail:
 python3 $TRAIN_PY --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 15  \
                  --num_layers 2 --dropout 0.2 --vocab AUTO \
                  --features coco\:train2014+coco\:val2014-resnet152-max-normalize-default.lmdb \
@@ -24,6 +16,15 @@ python3 $TRAIN_PY --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 1
                  --rnn_hidden_init from_features \
                  --skip_start_token
 
+
+# The following should work:
+python3 $TRAIN_PY --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 15  \
+                 --num_layers 2 --dropout 0.2 --vocab AUTO \
+                 --features coco\:train2014+coco\:val2014-resnet152-max-normalize-default.lmdb \
+                 --validate coco:val2014 \
+                 --model_name EncoderDecoder/test/${MODEL} \
+                 --rnn_hidden_init from_features
+                
 
 # Infer model with skipped start token:
 python3 $INFER_PY --model models/EncoderDecoder/test/${MODEL}/ep12.model \
