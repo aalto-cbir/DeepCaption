c75e1c6db027e5c246be158b00897122a1ff8371
Arturs Polis <arturs.polis@helsinki.fi>
2018-11-12 12:44:01 +0200

    Make rnn-init-tests run on less batches

--- tests/rnn_init_tests.sh
+++ tests/rnn_init_tests.sh
@@ -23,9 +23,9 @@ python3 $TRAIN_PY --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 1
                  --features coco\:train2014+coco\:val2014-resnet152-max-normalize-default.lmdb \
                  --validate coco:val2014 \
                  --model_name EncoderDecoder/test/${MODEL} \
-                 --rnn_hidden_init from_features
+                 --rnn_hidden_init from_features --num_batches 10
                 
 
 # Infer model with skipped start token:
-python3 $INFER_PY --model models/EncoderDecoder/test/${MODEL}/ep12.model \
+python3 $INFER_PY --model models/EncoderDecoder/test/${MODEL}/ep1.model \
                   --dataset coco:val2014 --output_format json
