63c9fe31d42cb9b248b25a9696bb5512391a9463
Sjöberg Mats <mats.sjoberg@aalto.fi>
2018-11-28 14:00:02 +0200

    Plot cider with its own y-axis

--- plot_stats.py
+++ plot_stats.py
@@ -5,26 +5,34 @@ import json
 import matplotlib.pyplot as plt
 
 
-def plot_stats(label, stats, color):
+def plot_stats(label, stats, color, ax1, ax2):
     epochs = sorted([int(x) for x in stats.keys()])
 
     measures = list(stats[str(epochs[0])].keys())
 
     for measure in measures:
         vals = [stats[str(e)][measure] for e in epochs]
+        ax = ax1
         if measure == 'validation_loss':
             linestyle = '--'
         elif measure == 'validation_cider':
             linestyle = ':'
+            ax = ax2
+            ax2.set_ylabel('CIDEr')
         else:
             linestyle = '-'
-        plt.plot(epochs, vals, label='{}: {}'.format(label, measure), color=color,
-                 linestyle=linestyle, linewidth=3)
+        ax.plot(epochs, vals, label='{}: {}'.format(label, measure), color=color,
+                linestyle=linestyle, linewidth=3)
 
 
 def main(args):
     colors = ['blue', 'red', 'cyan', 'yellow', 'gray', 'black']
     labels = args.labels.split(',') if args.labels else []
+
+    ax1 = plt.subplot(111)
+    ax2 = ax1.twinx()
+    ax1.set_xlabel('epochs')
+    ax1.set_ylabel('loss')
 
     for i, filename in enumerate(args.files):
         with open(filename, 'r') as fp:
@@ -33,13 +41,13 @@ def main(args):
             else:
                 label = str(i+1)
                 print('{}: {}'.format(label, filename))
-            plot_stats(label, json.load(fp), colors[i % len(colors)])
+            plot_stats(label, json.load(fp), colors[i % len(colors)], ax1, ax2)
 
-    ax = plt.subplot(111)
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.65, box.height])
+    box = ax1.get_position()
+    ax1.set_position([box.x0, box.y0, box.width * 0.6, box.height])
 
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax1.legend(loc='center left', bbox_to_anchor=(1.2, 0.8))
+    ax2.legend(loc='center left', bbox_to_anchor=(1.2, 0.6))
     plt.show()
 
 
