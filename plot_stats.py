23fdcfcc3521154c1d19969628f2f09e920d6037
Sjöberg Mats <mats.sjoberg@aalto.fi>
2018-11-29 09:57:47 +0200

    CIDEr smoothing

--- plot_stats.py
+++ plot_stats.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 import json
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_stats(label, stats, color, ax1, ax2):
+def plot_stats(label, stats, color, ax1, ax2, args):
     epochs = sorted([int(x) for x in stats.keys()])
 
     measures = list(stats[str(epochs[0])].keys())
@@ -21,6 +21,22 @@ def plot_stats(label, stats, color, ax1, ax2):
             ax2.set_ylabel('CIDEr')
         else:
             linestyle = '-'
+
+        mparts = measure.split('_', maxsplit=1)
+        measure_name = mparts[1] if len(mparts) > 1 else measure
+        if measure_name == 'cider' and args.smooth_cider is not None:
+            w = args.smooth_cider
+            ext = w//2
+            k = np.ones(w)/w
+            # duplicate first and last element for convolution to be "valid"
+            vals_ext = np.concatenate((np.tile(vals[0], ext), vals, np.tile(vals[-1], ext)))
+            valsc = np.convolve(k, vals_ext, mode='valid')
+            # print(vals_ext)
+            # print(valsc)
+            assert len(valsc) == len(vals)
+            vals = valsc
+            print('Smoothed {} with moving average width {}.'.format(measure, w))
+
         ax.plot(epochs, vals, label='{}: {}'.format(label, measure), color=color,
                 linestyle=linestyle, linewidth=3)
 
@@ -41,7 +57,7 @@ def main(args):
             else:
                 label = str(i+1)
                 print('{}: {}'.format(label, filename))
-            plot_stats(label, json.load(fp), colors[i % len(colors)], ax1, ax2)
+            plot_stats(label, json.load(fp), colors[i % len(colors)], ax1, ax2, args)
 
     box = ax1.get_position()
     ax1.set_position([box.x0, box.y0, box.width * 0.6, box.height])
@@ -58,6 +74,7 @@ if __name__ == '__main__':
     parser.add_argument('files', type=str, nargs='+',
                         help='JSON file(s) with training stats')
     parser.add_argument('--labels', type=str, help='Labels for plot')
+    parser.add_argument('--smooth_cider', type=int)
     args = parser.parse_args()
 
     main(args)
