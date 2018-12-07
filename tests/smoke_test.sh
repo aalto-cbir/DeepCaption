3de83bf565f60b33cd51c2343b6af9df726eb0fc
Arturs Polis <arturs.polis@helsinki.fi>
2018-12-07 11:26:06 +0200

    cleaned up smoke tests

--- tests/smoke_test.sh
+++ tests/smoke_test.sh
@@ -3,9 +3,9 @@
 # Smoke test the training, inference and evaluation code to make sure that the
 # basic functionality is in place
 
-TRAIN_PY=$1
-INFER_PY=$2
-EVAL_COCO_PY=$3
+TRAIN_PY=$1 # ./train.py
+INFER_PY=$2 # ./infer.py 
+EVAL_COCO_PY=$3 # ./eval_coco.py
 
 # Use a temporary history file to record previous commands so that 
 # we can output test results:
@@ -67,36 +67,39 @@ trap print_results EXIT
 #exit
 
 # Basic training run:
-python3 $TRAIN_PY --num_batches 10 --model_name __test/simple --num_epochs 1
-append_command_to_log
 
 # Expected result: training script starts training on MS COCO Train 2014 dataset, 
 # each epoch only uses the first 10 batches for training.
-# The model is using internal ResNet-152 features, and trains for 5 epochs
-
-python3 $TRAIN_PY --validate coco:val2014 --num_batches 10 \
-                  --model_name __test/validation --num_epochs 1
+# The model is using internal ResNet-152 features, and trains for 5 epochs:
+$TRAIN_PY --dataset coco:train2014 --vocab AUTO \
+                  --num_batches 10 --model_name __test/simple --num_epochs 1 
 append_command_to_log
 
 # Expected result: same as above, but validation loss is also calculated for each epoch
-INIT=c_in12_rn152_pool5o_d_a.lmdb
-python3 $TRAIN_PY --validate coco:val2014 --features $INIT \
-                --num_batches 10 --num_epochs 1 --model_name __test/ext_features
+$TRAIN_PY --dataset coco:train2014 --validate coco:val2014 --vocab AUTO \
+                  --num_batches 10 --model_name __test/validation --num_epochs 1
 append_command_to_log
 
-# Expected result: same as above, but using external features.
+# Expected result: same as above, but using external features:
+INIT=c_in12_rn152_pool5o_d_a.lmdb
+$TRAIN_PY --dataset coco:train2014 --validate coco:val2014 --features $INIT \
+                  --vocab AUTO --num_batches 10 --num_epochs 1 \
+                  --model_name __test/ext_features
+append_command_to_log
 
-python3 $TRAIN_PY --validate coco:val2014 --features c_in12_rn152_pool5o_d_a.lmdb \
-                --model_name __test/coco_full --lr_schedule \
+
+# Expected result: Model trained on MS COCO dataset with validation loss approaching
+# around 2.06 around epochs 11-12
+$TRAIN_PY --dataset coco:train2014  --validate coco:val2014 --features $INIT \
+                --vocab AUTO --model_name __test/coco_full --lr_schedule \
                 --num_epochs 11 --num_workers 4 --num_layers 2\
                 --dropout 0.2
 append_command_to_log
-# Expected result: Model trained on MS COCO dataset with validation loss approaching
-# around 2.06 around epochs 11-12
+
 
 MODEL=models/__test/coco_full/ep11.model
 
-python3 $INFER_PY --model $MODEL --dataset coco:val2014 --num_workers 4 \
+$INFER_PY --model $MODEL --dataset coco:val2014 --num_workers 4 \
                   --output_format json
 append_command_to_log
 # Expected result: reasonable looking results in results/__test_coco_full_ep11.json
@@ -104,7 +107,7 @@ append_command_to_log
 RESULTS=results/__test/coco_full-ep11.json
 GROUND_TRUTH=/m/cs/scratch/imagedb/picsom/databases/COCO/download/annotations/captions_val2014.json
 #GROUND_TRUTH=/proj/mediaind/picsom/databases/COCO/download/annotations/captions_val2014.json
-python2 $EVAL_COCO_PY $RESULTS --ground_truth $GROUND_TRUTH
+$EVAL_COCO_PY $RESULTS --ground_truth $GROUND_TRUTH
 append_command_to_log
 # Expected results: Meteor close to 0.240, CIDEr close to: 0.853
 
@@ -112,20 +115,20 @@ append_command_to_log
 INIT=c_in12_rn101_pool5o_d_a.lmdb,c_in12_rn152_pool5o_d_a.lmdb
 #PERS=fasterRcnn_clasDetFEat80.lmdb,fasterRcnn_spatMapFeat3+3GaussScaleDet.lmdb,f::6gr::RBF::sun-397.lmdb
 
-python3 $TRAIN_PY --dataset coco:train2014+msrvtt:train \
+$TRAIN_PY --dataset coco:train2014+msrvtt:train \
           --model_name __test/coco+msrvtt-rn \
           --embed_size=512 --hidden_size=1024 \
           --num_layers=2 --dropout=0.5 --encoder_dropout=0.5 \
           --optimizer=rmsprop \
-          --validate msrvtt:validate --vocab_path vocab-coco+msrvtt.pkl \
+          --validate msrvtt:validate --vocab vocab-coco+msrvtt.pkl \
           --features $INIT \
           --num_workers 2 --num_epochs=11
 append_command_to_log
 
 MODEL=models/__test/coco+msrvtt-rn/ep11.model
 
-python3 $INFER_PY --model $MODEL --dataset trecvid2018 \
-         --vocab_path vocab/vocab-coco+msrvtt.pkl \
+$INFER_PY --model $MODEL --dataset trecvid2018 \
+         --vocab vocab/vocab-coco+msrvtt.pkl \
          --output_format json
 append_command_to_log
 
