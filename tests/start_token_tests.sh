4706a09e73d6a58f7cd04cb0ee15cb89e9182dfa
Arturs Polis <arturs.polis@helsinki.fi>
2018-12-07 13:59:03 +0200

    added smoke test for testing without start token

--- tests/start_token_tests.sh
+++ tests/start_token_tests.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Train baseline model without start-token:
 
+# Smoke test training without start token:
+MODEL="__test/coco-max-default-nl2-skip_start_token_smoke_test"
+./train.py --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 5  \
+                 --dataset coco:train2014 --num_layers 2 --dropout 0.2 --vocab AUTO \
+                 --features coco\:train2014+coco\:val2014-resnet152-max-normalize-default.lmdb \
+                 --validate coco:val2014 --num_batches 10 \
+                 --model_name EncoderDecoder/test/${MODEL} \
+                 --skip_start_token --validation_scoring cider
+
+# Infer model with skipped start token:
+./infer.py --model models/EncoderDecoder/test/${MODEL}/ep5.model \
+                  --dataset coco:val2014 --output_format json
+
+# Train full baseline model without start token:
 MODEL="__test/coco-max-default-nl2-skip_start_token"
 ./train.py --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 15  \
                  --dataset coco:train2014 --num_layers 2 --dropout 0.2 --vocab AUTO \
