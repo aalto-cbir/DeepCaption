c9c7397f087ad98b0335d58fec802ec7104a390c
Arturs Polis <arturs.polis@helsinki.fi>
2018-12-11 15:46:06 +0200

    reduced number of epochs trained for start_token_test

--- tests/start_token_tests.sh
+++ tests/start_token_tests.sh
@@ -26,7 +26,7 @@ append_command_to_log
 if [ -z $SKIP_LONG_COMMANDS ]; then
     # Train full baseline model without start token:
     MODEL="__test/coco-max-default-nl2-skip_start_token"
-    ./train.py --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 15  \
+    ./train.py --num_workers 4 --lr_schedule  --optimizer adam --num_epochs 11  \
                      --dataset coco:train2014 --num_layers 2 --dropout 0.2 --vocab AUTO \
                      --features coco\:train2014+coco\:val2014-resnet152-max-normalize-default.lmdb \
                      --validate coco:val2014 \
@@ -36,6 +36,6 @@ if [ -z $SKIP_LONG_COMMANDS ]; then
 fi
 
 # Infer model with skipped start token:
-./infer.py --model models/EncoderDecoder/test/${MODEL}/ep12.model \
+./infer.py --model models/EncoderDecoder/test/${MODEL}/ep11.model \
                   --dataset coco:val2014 --output_format json
 append_command_to_log
