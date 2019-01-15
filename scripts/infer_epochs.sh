84a13124df074b9cadd1dde59fff2f2ba1a8186f
Arturs Polis <arturs.polis@helsinki.fi>
2019-01-15 17:03:42 +0200

    Skip models that don\'t exist when doing batch inference

--- scripts/infer_epochs.sh
+++ scripts/infer_epochs.sh
@@ -10,6 +10,12 @@ echo "Epoch start: $EP_BEGIN"
 echo "Epoch end: $EP_END"
 
 for EPOCH in $(eval echo {$EP_BEGIN..$EP_END}); do
-    python3 infer.py --model "$MODEL_PATH/ep${EPOCH}.model" --dataset $DATASET \
-                     --output_format json --num_workers 4
+    MODEL_FILE="$MODEL_PATH/ep${EPOCH}.model"
+    if [ -f $MODEL_FILE ]; then
+        echo "Inferring from model: $MODEL_FILE using dataset: $DATASET..."
+        python3 infer.py --model $MODEL_FILE --dataset $DATASET \
+                         --output_format json --num_workers 4
+    else
+        echo "File $MODEL_FILE doesn't exist, skipping."
+    fi
 done
