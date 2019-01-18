32539849b76eaa79e496d0271e479759d1d67cd2
Arturs Polis <arturs.polis@helsinki.fi>
2019-01-18 10:07:00 +0200

    Infer script now supports arbitrary pass-thru params

--- scripts/infer_epochs.sh
+++ scripts/infer_epochs.sh
@@ -4,6 +4,9 @@ MODEL_PATH=$1
 DATASET=$2
 EP_BEGIN=$3
 EP_END=$4
+# The line below allows user to supply arbitrary many arguments starting with $5
+# that can be supplied directly to the ./infer.py script:
+MISC_PARAMS=${@:5}
 
 echo "Evaluating model: $MODEL_PATH"
 echo "Epoch start: $EP_BEGIN"
@@ -14,7 +17,7 @@ for EPOCH in $(eval echo {$EP_BEGIN..$EP_END}); do
     if [ -f $MODEL_FILE ]; then
         echo "Inferring from model: $MODEL_FILE using dataset: $DATASET..."
         python3 infer.py --model $MODEL_FILE --dataset $DATASET \
-                         --output_format json --num_workers 4
+                         --output_format json --num_workers 4 $MISC_PARAMS
     else
         echo "File $MODEL_FILE doesn't exist, skipping."
     fi
