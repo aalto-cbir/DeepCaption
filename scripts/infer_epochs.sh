b412f43d06fcdc1c413bf905b90c6927eec23f92
Arturs Polis <arturs.polis@helsinki.fi>
2019-01-21 15:40:42 +0200

    Improved Eval and infer scripts

--- scripts/infer_epochs.sh
+++ scripts/infer_epochs.sh
@@ -14,7 +14,16 @@ echo "Epoch end: $EP_END"
 
 for EPOCH in $(eval echo {$EP_BEGIN..$EP_END}); do
     MODEL_FILE="$MODEL_PATH/ep${EPOCH}.model"
-    if [ -f $MODEL_FILE ]; then
+    MODEL_NAME=$(basename $MODEL_PATH)
+    
+    RESULTS_FILE="results/${MODEL_NAME}-ep${EPOCH}.json"
+    
+    if [ -s $RESULTS_FILE ]; then
+        echo "File $RESULTS_FILE already exists. Please remove it if you want to infer this model again"
+        continue
+    fi
+
+    if [ -s $MODEL_FILE ]; then
         echo "Inferring from model: $MODEL_FILE using dataset: $DATASET..."
         python3 infer.py --model $MODEL_FILE --dataset $DATASET \
                          --output_format json --num_workers 4 $MISC_PARAMS
